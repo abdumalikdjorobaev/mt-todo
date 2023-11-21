d150b759a6a41a4183f5b2e7ddbc132851a0f0b6
Abdumalik <abdumalikmacbook@gmail.com>
2023-11-21 14:17:13 +0600

    dqwdwqd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { TodoList } from "./pages/Todo";
+
+import '../src/styles/index.scss'
+
+function App() {
+  return <div className="App">
+    <TodoList/>
+  </div>;
+}
+
+export default App;
